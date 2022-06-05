12b2ef876b6dd28c7cba53d566021597e090c2e5
Anany09876 <96874243+Anany09876@users.noreply.github.com>
2022-06-05 16:15:22 +0530

    Update callback.py

--- m8n/modules/callback.py
+++ m8n/modules/callback.py
@@ -173,13 +173,9 @@ Click on the given inline buttons to know all the information about the Bot !!""
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("📨 Support", url=f"https://t.me/{SUPPORT}"),
-                    InlineKeyboardButton("📨 Updates", url=f"https://t.me/{UPDATE}")
+                    InlineKeyboardButton("✨ Support", url=f"https://t.me/{SUPPORT}"),
                 ],[
                     InlineKeyboardButton("👤 Owner", url=f"https://t.me/{OWNER_USERNAME}"),
-                    InlineKeyboardButton("🎸 Assistant", url=f"https://t.me/{ASSUSERNAME}")
-                ],[
-                    InlineKeyboardButton("🤖 Source Code", url="https://github.com/UnknownMortal/M8N-Music-Bot")
                 ],[
                     InlineKeyboardButton("⬅️ Back", callback_data="cbhome")
                 ],
